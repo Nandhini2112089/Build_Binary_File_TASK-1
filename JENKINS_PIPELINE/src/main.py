7a8d87005178b96043358b430a534f4664854adc
Siva Nandhini B <114659701+Nandhini2112089@users.noreply.github.com>
2025-05-13 15:48:01 +0530

    Create main.py

---
+++ JENKINS_PIPELINE/src/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app import app 
+def main():
+    uvicorn.run("app:app", host="0.0.0.0", port=8081)
+
+if __name__ == "__main__":
+    main()
+  
