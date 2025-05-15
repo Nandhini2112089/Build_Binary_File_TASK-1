bee481b4b7cc15a2fc9affb3bc9056d89510b464
Siva Nandhini B <114659701+Nandhini2112089@users.noreply.github.com>
2025-05-15 10:37:47 +0530

    Delete JENKINS_PIPELINE directory

--- JENKINS_PIPELINE/lib/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from models import PasswordRequest  
-from validator import validate_password  
-
-router = APIRouter()
-
-@router.post("/verify-password/")
-def verify_password(data: PasswordRequest):
-    return validate_password(data.password)
